e1d804a80b2543d70c90b35f31683853ecba023c
Greg Richardson <greg.nmr@gmail.com>
2016-02-01 19:13:41 -0800

    Removed debug code from workload generator

--- workload-generator/src/main/java/com/seng462ged/daytrader/workloadgenerator/Main.java
+++ workload-generator/src/main/java/com/seng462ged/daytrader/workloadgenerator/Main.java
@@ -2,7 +2,6 @@ package com.seng462ged.daytrader.workloadgenerator;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -19,25 +18,14 @@ public class Main {
 
             List<Transaction> transactions = Importer.ImportTransactions(workloadFile);
 
-            for (Transaction transaction : transactions) {
-
-                System.out.println(transaction.getId());
-                System.out.println(transaction.getCommand());
-                System.out.println(transaction.getUserId());
-                System.out.println(transaction.getStockSymbol());
-                System.out.println(transaction.getAmount());
-                System.out.println(transaction.getFilename());
-                System.out.println();
-            }
-
-            //List<Transaction> transactions2 = new ArrayList<Transaction>();
-            //transactions2.add(transactions.get(0));
-
             Requester.SendTransactions(transactions);
 
         } catch (FileNotFoundException e) {
+
             System.out.println(String.format("Could not find file '%s'", workloadFile));
+
         } catch (IOException e) {
+
             System.out.println(String.format("There were problems reading file '%s'", workloadFile));
         }
     }
