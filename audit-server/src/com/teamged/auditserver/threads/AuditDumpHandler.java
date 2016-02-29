817a57616f4cd37a569d2958113679686956cea1
esmurphy7 <esmurphy7@gmail.com>
2016-02-28 21:42:55 -0800

    [audit-server] implemented single audit dump thread and handler that saves the logfile when signalled

--- audit-server/src/com/teamged/auditserver/threads/AuditDumpHandler.java
+++ audit-server/src/com/teamged/auditserver/threads/AuditDumpHandler.java
@@ -1,7 +1,39 @@
 package com.teamged.auditserver.threads;
 
+import com.teamged.auditserver.InternalLog;
+import com.teamged.logging.Logger;
+
+import javax.xml.bind.JAXBException;
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by DanielF on 2016-02-23.
  */
-public class AuditDumpHandler {
+public class AuditDumpHandler implements Runnable
+{
+    private final Socket socket;
+
+    public AuditDumpHandler(Socket socket)
+    {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run()
+    {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())))
+        {
+            String message = in.readLine();
+            if( message != null && message.equals("DUMPLOG"))
+            {
+                Logger.getInstance().SaveLog();
+            }
+        } catch (IOException | JAXBException e) {
+            e.printStackTrace();
+            return;
+        }
+
+
+    }
 }
