ceaadede96eba467de91f54830d2fbbb7f4a53da
Greg Richardson <greg.nmr@gmail.com>
2016-03-01 16:19:42 -0800

    Modified run-nginx.sh to allow argument passing

--- deploy-script/src/main/resources/run-nginx.sh
+++ deploy-script/src/main/resources/run-nginx.sh
@@ -3,4 +3,4 @@
 installDirectory=/seng/scratch/group4
 programName=nginx
 
-$installDirectory/$programName/sbin/nginx -c $installDirectory/$programName/conf/nginx.conf.loadbalancer
+$installDirectory/$programName/sbin/nginx -c $installDirectory/$programName/conf/nginx.conf.loadbalancer "$@"
