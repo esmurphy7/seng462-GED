ab95c3e0db7aa6b6c71c59307aa4be4be32448f7
esmurphy7 <esmurphy7@gmail.com>
2016-03-03 15:14:26 -0800

    [audit-server] clear logs when dumplog received

--- audit-server/src/com/teamged/auditserver/threads/AuditDumpHandler.java
+++ audit-server/src/com/teamged/auditserver/threads/AuditDumpHandler.java
@@ -22,18 +22,18 @@ public class AuditDumpHandler implements Runnable
     @Override
     public void run()
     {
+        // when a dumplog is signalled, save the logs to a file and clear the list
         try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())))
         {
             String message = in.readLine();
             if( message != null && message.equals("DUMPLOG"))
             {
                 Logger.getInstance().SaveLog();
+                Logger.getInstance().ClearLogs();
             }
         } catch (IOException | JAXBException e) {
             e.printStackTrace();
             return;
         }
-
-
     }
 }
