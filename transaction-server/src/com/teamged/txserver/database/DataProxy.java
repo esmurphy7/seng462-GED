317edc199158434ae47681538d3762b15783e2ee
Daniel Faulkner <danielafaulkner@gmail.com>
2016-03-08 18:24:36 -0800

    Prevent data proxy failure on user db object error

--- transaction-server/src/com/teamged/txserver/database/DataProxy.java
+++ transaction-server/src/com/teamged/txserver/database/DataProxy.java
@@ -24,91 +24,95 @@ public class DataProxy {
     private static final ConcurrentHashMap<String, UserDatabaseObject> dbInterfaces = new ConcurrentHashMap<>();
 
     public static String dbOperation(TransactionObject tx) {
-        InternalLog.Log(tx.getUserName() + " - " + tx.getUserSeqNum() + " - " + tx.getWorkloadSeqNum() + " - " + tx.getUserCommand().toString());
-        String opResult;
-        if (tx.getUserCommand().equals(UserCommand.DUMPLOG_ROOT)) {
-            opResult = "DUMPLOG_ROOT";
-            Logger.getInstance().SaveLog(tx.getWorkloadSeqNum());
-            System.out.println("RECEIVED DUMPLOG_ROOT COMMAND!");
-        } else {
-
-            UserDatabaseObject dbProxy = getDBProxy(tx.getUserName());
-            if (dbProxy != null) {
-
-                // Log the database connection as a system event
-                SystemEventType systemEvent = new SystemEventType();
-
-                systemEvent.setTimestamp(System.currentTimeMillis());
-                systemEvent.setServer(ServerConstants.TX_SERVERS[0]);
-                systemEvent.setTransactionNum(BigInteger.valueOf(tx.getWorkloadSeqNum()));
-                systemEvent.setUsername(tx.getUserName());
-                systemEvent.setCommand(CommandType.fromValue(tx.getUserCommand().name()));
-                systemEvent.setStockSymbol(tx.getStockSymbol());
-                systemEvent.setFilename(tx.getFileName());
-
-                Logger.getInstance().Log(systemEvent);
-
-                switch (tx.getUserCommand()) {
-                    case NO_COMMAND:
-                        opResult = "NO_COMMAND";
-                        break;
-                    case ADD:
-                        opResult = dbProxy.add(tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
-                        break;
-                    case QUOTE:
-                        opResult = dbProxy.quote(tx.getStockSymbol(), tx.getWorkloadSeqNum()).toString();
-                        break;
-                    case BUY:
-                        opResult = dbProxy.buy(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
-                        break;
-                    case COMMIT_BUY:
-                        opResult = dbProxy.commitBuy(tx.getWorkloadSeqNum());
-                        break;
-                    case CANCEL_BUY:
-                        opResult = dbProxy.cancelBuy(tx.getWorkloadSeqNum());
-                        break;
-                    case SELL:
-                        opResult = dbProxy.sell(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
-                        break;
-                    case COMMIT_SELL:
-                        opResult = dbProxy.commitSell(tx.getWorkloadSeqNum());
-                        break;
-                    case CANCEL_SELL:
-                        opResult = dbProxy.cancelSell(tx.getWorkloadSeqNum());
-                        break;
-                    case SET_BUY_AMOUNT:
-                        opResult = dbProxy.setBuyAmount(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
-                        break;
-                    case CANCEL_SET_BUY:
-                        opResult = dbProxy.cancelSetBuy(tx.getStockSymbol(), tx.getWorkloadSeqNum());
-                        break;
-                    case SET_BUY_TRIGGER:
-                        opResult = dbProxy.setBuyTrigger(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
-                        break;
-                    case SET_SELL_AMOUNT:
-                        opResult = dbProxy.setSellAmount(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
-                        break;
-                    case SET_SELL_TRIGGER:
-                        opResult = dbProxy.setSellTrigger(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
-                        break;
-                    case CANCEL_SET_SELL:
-                        opResult = dbProxy.cancelSetSell(tx.getStockSymbol(), tx.getWorkloadSeqNum());
-                        break;
-                    case DUMPLOG:
-                        opResult = dbProxy.dumplog(tx.getFileName(), tx.getWorkloadSeqNum());
-                        break;
-                    case DISPLAY_SUMMARY:
-                        opResult = dbProxy.displaySummary(tx.getWorkloadSeqNum());
-                        break;
-                    case DUMPLOG_ROOT:
-                    default:
-                        opResult = "Unknown command \"" + tx.getUserCommand().toString() + "\" was given";
-                        break;
-                }
-
+        String opResult = "";
+        try {
+            InternalLog.Log(tx.getUserName() + " - " + tx.getUserSeqNum() + " - " + tx.getWorkloadSeqNum() + " - " + tx.getUserCommand().toString());
+            if (tx.getUserCommand().equals(UserCommand.DUMPLOG_ROOT)) {
+                opResult = "DUMPLOG_ROOT";
+                Logger.getInstance().SaveLog(tx.getWorkloadSeqNum());
+                System.out.println("RECEIVED DUMPLOG_ROOT COMMAND!");
             } else {
-                opResult = "ERROR," + tx.toString();
+
+                UserDatabaseObject dbProxy = getDBProxy(tx.getUserName());
+                if (dbProxy != null) {
+
+                    // Log the database connection as a system event
+                    SystemEventType systemEvent = new SystemEventType();
+
+                    systemEvent.setTimestamp(System.currentTimeMillis());
+                    systemEvent.setServer(ServerConstants.TX_SERVERS[0]);
+                    systemEvent.setTransactionNum(BigInteger.valueOf(tx.getWorkloadSeqNum()));
+                    systemEvent.setUsername(tx.getUserName());
+                    systemEvent.setCommand(CommandType.fromValue(tx.getUserCommand().name()));
+                    systemEvent.setStockSymbol(tx.getStockSymbol());
+                    systemEvent.setFilename(tx.getFileName());
+
+                    Logger.getInstance().Log(systemEvent);
+
+                    switch (tx.getUserCommand()) {
+                        case NO_COMMAND:
+                            opResult = "NO_COMMAND";
+                            break;
+                        case ADD:
+                            opResult = dbProxy.add(tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
+                            break;
+                        case QUOTE:
+                            opResult = dbProxy.quote(tx.getStockSymbol(), tx.getWorkloadSeqNum()).toString();
+                            break;
+                        case BUY:
+                            opResult = dbProxy.buy(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
+                            break;
+                        case COMMIT_BUY:
+                            opResult = dbProxy.commitBuy(tx.getWorkloadSeqNum());
+                            break;
+                        case CANCEL_BUY:
+                            opResult = dbProxy.cancelBuy(tx.getWorkloadSeqNum());
+                            break;
+                        case SELL:
+                            opResult = dbProxy.sell(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
+                            break;
+                        case COMMIT_SELL:
+                            opResult = dbProxy.commitSell(tx.getWorkloadSeqNum());
+                            break;
+                        case CANCEL_SELL:
+                            opResult = dbProxy.cancelSell(tx.getWorkloadSeqNum());
+                            break;
+                        case SET_BUY_AMOUNT:
+                            opResult = dbProxy.setBuyAmount(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
+                            break;
+                        case CANCEL_SET_BUY:
+                            opResult = dbProxy.cancelSetBuy(tx.getStockSymbol(), tx.getWorkloadSeqNum());
+                            break;
+                        case SET_BUY_TRIGGER:
+                            opResult = dbProxy.setBuyTrigger(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
+                            break;
+                        case SET_SELL_AMOUNT:
+                            opResult = dbProxy.setSellAmount(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
+                            break;
+                        case SET_SELL_TRIGGER:
+                            opResult = dbProxy.setSellTrigger(tx.getStockSymbol(), tx.getAmountDollars(), tx.getAmountCents(), tx.getWorkloadSeqNum());
+                            break;
+                        case CANCEL_SET_SELL:
+                            opResult = dbProxy.cancelSetSell(tx.getStockSymbol(), tx.getWorkloadSeqNum());
+                            break;
+                        case DUMPLOG:
+                            opResult = dbProxy.dumplog(tx.getFileName(), tx.getWorkloadSeqNum());
+                            break;
+                        case DISPLAY_SUMMARY:
+                            opResult = dbProxy.displaySummary(tx.getWorkloadSeqNum());
+                            break;
+                        case DUMPLOG_ROOT:
+                        default:
+                            opResult = "Unknown command \"" + tx.getUserCommand().toString() + "\" was given";
+                            break;
+                    }
+
+                } else {
+                    opResult = "ERROR," + tx.toString();
+                }
             }
+        } catch (Exception e) {
+            System.out.println("[ERROR CAUGHT IN DATA PROXY] " + e.getMessage());
         }
 
         Logger.getInstance().Log("IDX," + tx.getWorkloadSeqNum());
