aebd6dc22b165bd36e8b287bc54c3e20ee9da359
esmurphy7 <esmurphy7@gmail.com>
2016-03-09 16:08:01 -0800

    [deploy-script] update run-webserver script to copy config.json into tomcat installation

--- deploy-script/src/main/resources/run-web-server.sh
+++ deploy-script/src/main/resources/run-web-server.sh
@@ -3,4 +3,8 @@
 installDirectory=/seng/scratch/group4
 programName=tomcat
 
+# move the config.json file into the tomcat installation for web server to reference
+mv $installDirectory/config.json $installDirectory/$programName/bin
+
+# run the tomcat startup script
 $installDirectory/$programName/bin/startup.sh
