897c2862c2cf5bf49f7551a99997ef9e67180315
Daniel Faulkner <danielafaulkner@gmail.com>
2016-03-09 22:52:19 -0800

    Removed outdated unused log queue

--- audit-server/src/main/java/com/teamged/auditserver/AuditMain.java
+++ audit-server/src/main/java/com/teamged/auditserver/AuditMain.java
@@ -1,37 +1,30 @@
 package com.teamged.auditserver;
 
 import com.teamged.ServerConstants;
+import com.teamged.auditlogging.LogManager;
 import com.teamged.auditserver.threads.AuditDumpThread;
 import com.teamged.auditserver.threads.AuditServerThread;
 import com.teamged.auditserver.threads.LogConnectionThread;
-import com.teamged.auditlogging.LogManager;
 import com.teamged.deployment.DeployParser;
 import com.teamged.deployment.DeploymentSettings;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.LongAdder;
 
 /**
  * Created by DanielF on 2016-02-23.
  */
 public class AuditMain {
+    private static final Object dumpLockObject = new Object();
+    private static final Object syncObject = new Object();
+    private static final ArrayList<AuditServerThread> auditConnThreads = new ArrayList<>();
+    private static final ArrayList<AuditServerThread> auditDumpThreads = new ArrayList<>();
     public static DeploymentSettings Deployment;
-
     private static String hostname;
     private static LongAdder userSequenceTotal = new LongAdder();
     private static boolean dumpReady = false;
     private static long expectedSequenceTotal = 0;
-    private static final Object dumpLockObject = new Object();
-
-    private static final Object syncObject = new Object();
-
-    private static final ArrayList<AuditServerThread> auditConnThreads = new ArrayList<>();
-    private static final ArrayList<AuditServerThread> auditDumpThreads = new ArrayList<>();
-
-    private static final Queue<String> auditQueue = new ConcurrentLinkedQueue<>();
 
     public static void main(String[] args) {
         parseArgs(args);
@@ -110,49 +103,16 @@ public class AuditMain {
      * request have all arrived.
      */
     public static void dumpIfReady() {
-            synchronized (dumpLockObject) {
-                if (dumpIsReady()) {
-                    dumpReady = false;
-                    expectedSequenceTotal = 0;
-                    userSequenceTotal.reset();
-                    InternalLog.Log("Beginning log dump!");
-                    LogManager.DumpLog();
-                    InternalLog.Log("Log dump ended!");
-                }
+        synchronized (dumpLockObject) {
+            if (dumpIsReady()) {
+                dumpReady = false;
+                expectedSequenceTotal = 0;
+                userSequenceTotal.reset();
+                InternalLog.Log("Beginning log dump!");
+                LogManager.DumpLog();
+                InternalLog.Log("Log dump ended!");
             }
-    }
-
-    /**
-     * Puts a serialized log into the queue. When the logs are dumped, it will be sanitized and written
-     * to a file.
-     *
-     * @param log The self-contained log to store.
-     */
-    public static void putLogQueue(String log) {
-        try {
-            auditQueue.add(log);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-            InternalLog.Log("Attempted to add null log to log queue");
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            InternalLog.Log("Log experienced an unexpected error while queueing: " + log);
         }
-    }
-
-    /**
-     * Takes a log from the top of the log queue. If the queue is empty, this will return null.
-     *
-     * @return The log at the top of the queue, or null if the queue is empty.
-     */
-    public static String takeLogQueue() {
-        String log = null;
-        try {
-            log = auditQueue.remove();
-        } catch (Exception e) {
-        }
-
-        return log;
     }
 
     private static void parseArgs(String[] args) {
